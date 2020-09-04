331d861a49a1442b0e2866a75ddfe230616a3913
张晓天 <zhangxiaotian@citytogo.com>
2020-09-04 18:16:27 +0800

    debug

--- src/com/company/merge_sorted_array/Three.java
+++ src/com/company/merge_sorted_array/Three.java
@@ -6,27 +6,37 @@ public class Three {
     //空间复杂度可以再优化
     public void solution1(int[] nums1, int m, int[] nums2, int n) {
         int[] nums1_copy = new int[m];
+        System.arraycopy(nums1, 0, nums1_copy, 0, m);
 
         int point1 = 0;
         int point2 = 0;
         int index = 0;
         while (point1 < m && point2 < n) {
-            nums1_copy[index++] = nums1[point1] < nums2[point2] ? nums1[point1++] : nums2[point2++];
+            nums1[index++] = nums1_copy[point1] < nums2[point2] ? nums1_copy[point1++] : nums2[point2++];
         }
 
         if (point1 < m) {
-            System.arraycopy(nums1, point1, nums1_copy, index, m - point1);
+            System.arraycopy(nums1_copy, point1, nums1, index, m - point1);
         }
 
         if (point2 < n) {
-            System.arraycopy(nums2, point2, nums1_copy, index, n - point2);
+            System.arraycopy(nums2, point2, nums1, index, n - point2);
         }
-
-        System.arraycopy(nums1_copy, 0, nums1, 0, m);
     }
 
     //将归并结果放入其中一个数组，不需要额外的空间
-    public int[] solution2(int[] nums1, int m, int[] nums2, int n) {
+    public void solution2(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int index = m + n - 1;
+        //循环比较两个数组的最大值
+        // 写入nums1末尾
+        while (p1 >= 0 && p2 >= 0) {
+            nums1[index--] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
+        }
 
+        if (p2 >= 0) {
+            System.arraycopy(nums2, 0, nums1, 0, index + 1);
+        }
     }
 }
